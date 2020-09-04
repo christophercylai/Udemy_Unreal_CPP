60f8a2758cc87d97edb041021394e4862088302b
christophercylai <christophercylai@outlook.com>
2020-09-03 19:39:51 -0700

    added triple_x project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# TripleX
-[Udemy] Unreal Engine C++ Developer: Learn C++ and Make Video Games - TripleX Project
+# [Udemy] Unreal Engine C++ Developer: Learn C++ and Make Video Games Projects
+
+## Installation
+* required: Visual Studio 2019 (Community Edition) and install the following options:
+  * "Game development with C++"" with "Unreal Engine installer"
+* optional: Visual Studio Code for editing, and add the "C/C++" Extension
---
+++ triple_x/triple_x.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <ctime>
+// TripleX
+
+
+int rand_num_gen(int *level) {
+    // helper function that generates a random int based of level
+    return rand() % (*level + 1);
+}
+
+
+bool combination(int *level, int *tries) {
+    /*
+    * the combination game, return 0 if the combination is incorrect
+    * else return next_level = ++level
+    */
+    // setup the secret combinations with random numbers
+    srand(time(NULL));  // create new random sequence based on time of day
+    int code_a = rand_num_gen(level),
+        code_b = rand_num_gen(level),
+        code_c = rand_num_gen(level);
+
+    int code_sum = code_a + code_b + code_c;
+    int code_product = code_a * code_b * code_c;
+    int guess_a = 0, guess_b = 0, guess_c =0, guess_sum = 0, guess_product = 0;
+
+    while (*tries > 0) {
+        // ask for the user's combination inputs
+        std::cout << "### Level " << *level << " ###" << std::endl;
+        std::cout << "The sum of the combination is: " << code_sum << std::endl;
+        std::cout << "The product of the combination is: " << code_product << std::endl;
+        std::cout << "You need to enter the correct combinations to continue" << std::endl;
+        std::cout << "Enter combinations ... (e.g. 5 16 2)" << std::endl;
+        std::cin >> guess_a;
+        std::cin >> guess_b;
+        std::cin >> guess_c;
+
+        // check whether the user has guessed the combinations correctly
+        guess_sum = guess_a + guess_b + guess_c;
+        guess_product = guess_a * guess_b * guess_c;
+
+        if (guess_sum != code_sum || guess_product != code_product) {
+            --*tries;
+            std::cout << "Combinations incorrect - Remaining chances: " << *tries << "\n" << std::endl;
+        } else {
+            ++*level;
+            std::cout << "The combinations you entered is correct" << std::endl;
+            std::cout << std::endl;
+            return true;
+        }
+    }
+
+    std::cout << "\n*** Mission failed :( ***\n" << std::endl;
+    return false;
+}
+
+
+int main() {
+    /*
+     * TripleX - guess combinations and get into the server room
+    */
+    // print storyline messages
+    const int max_level = 10;
+    std::cout << "\n\nYou are a secret agent breaking into a secure server room." << std::endl;
+    std::cout <<
+        "There are " << max_level << " levels, and each requires 3 combinations to unlock ...\n\n"
+    << std::endl;
+
+    int level = 1, tries = 3;
+    bool success = true;
+    while (level <= max_level) {
+        success = combination(&level, &tries);
+        // clean up stdin buffer
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (success == false) {
+            goto EXIT;
+        }
+    }
+    std::cout << "\n*** You have successfully entered the server room! ***\n" << std::endl;
+
+    EXIT:return 0;
+}
+
